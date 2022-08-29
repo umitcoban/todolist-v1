3fda65945dabd0beec8fb4d0facd9572142978c0
Ümit Yasin Çoban <umityasincoban@gmail.com>
2022-08-30 01:32:27 +0300

    add and setup ejs module

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
    var today = new Date();
-   if(today.getDay()===2){
-    res.send("WEEKEND!!");
-   }else{
-    res.send("SORRY :(");
+   var currentDay = today.getDay();
+    var day = "";
+   switch(currentDay){
+    case 0:
+        day="sunday";
+        break;
+        case 1:
+            day="monday";
+            break;
+            case 2:
+                day="tuesday";
+                break;
+                case 3:
+                    day="wednesday";
+                    break;
+                    case 4:
+                        day="thursday";
+                        break;
+                        case 5:
+                            day="friday";
+                            break;
+                            case 6:
+                                day="saturday";
+                                break;
+                                default:
+                                    console.log("Error Current Day: " + currentDay);
    }
+    res.render('list', {kindOfDay: day.toLocaleUpperCase()});
 });
 
 
