cfea150b2951aca46cd2f92b597825502a25532f
Ümit Yasin Çoban <umityasincoban@gmail.com>
2022-08-30 15:56:18 +0300

    Create layout, styles, todo and work logic

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+const toDoList = ["Buy Food", "Cook Food", "Eat Food"];
+const workList = [];
 
-app.get("/", function(req,res){
-   var today = new Date();
-   var currentDay = today.getDay();
-    var day = "";
-   switch(currentDay){
-    case 0:
-        day="sunday";
-        break;
-        case 1:
-            day="monday";
-            break;
-            case 2:
-                day="tuesday";
-                break;
-                case 3:
-                    day="wednesday";
-                    break;
-                    case 4:
-                        day="thursday";
-                        break;
-                        case 5:
-                            day="friday";
-                            break;
-                            case 6:
-                                day="saturday";
-                                break;
-                                default:
-                                    console.log("Error Current Day: " + currentDay);
-   }
-    res.render('list', {kindOfDay: day.toLocaleUpperCase()});
+
+app.get("/", function (req, res) {
+    const day = date.getDate();
+
+
+    res.render('list', { listTitle: day.toLocaleUpperCase(), todolist: toDoList });
 });
 
+app.post("/", function (req, res) {
+    const newTodo = req.body.todo;
 
-app.listen(3000, function () {  
+    if (req.body.list.toLowerCase() === "work".toLowerCase()) {
+        workList.push(newTodo);
+        res.redirect("/work");
+    } else {
+        toDoList.push(newTodo);
+        res.redirect("/");
+    }
+    console.log(req.body);
+
+});
+
+app.get("/work", function (req, res) {
+    let title = "work list";
+    res.render('list', { listTitle: title.toUpperCase(), todolist: workList });
+});
+
+app.post("/work", function (req, res) {
+    const newWork = req.body.todo;
+    workList.push(newWork);
+    res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+app.listen(3000, function () {
     console.log("server has started 3000");
 });
